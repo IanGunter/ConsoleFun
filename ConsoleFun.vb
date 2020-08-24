d4f9c0781e098066cfe68d20ea61339c87669c75
Ian Gunter <iangunterofamerica@yahoo.com>
2020-08-24 08:38:57 -0600

    Add Convert String to Integer

--- ConsoleFun.vb
+++ ConsoleFun.vb
@@ -1,13 +1,23 @@
-﻿
+﻿Option Explicit On
 Option Strict On
 
 Module ConsoleFun
 
     Sub Main()
         Dim userInput As String
-        Console.WriteLine("Please Type Some Text Then Press Enter...")
+        'Console.WriteLine("Please Type Some Text Then Press Enter...")
+        'userInput = Console.ReadLine()
+        'Console.WriteLine("Thanks!! You entered: " & userInput & ", Good Work!!")
+        'Console.ReadLine() 
+
+        Dim firstNumber As Integer
+        Dim Result As Integer
+
+        Console.WriteLine("Please Enter A Number")
         userInput = Console.ReadLine()
-        Console.WriteLine("Thanks!! You entered: " & userInput & ", Good Work!!")
+        firstNumber = CInt(userInput)
+        Result = 5 + firstNumber
+        Console.WriteLine(Result)
         Console.ReadLine()
 
 
